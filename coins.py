af57bee877ec658602bff7734af072cbe67d7571
RomanSC <r.s.col988@gmail.com>
2017-02-11 19:03:14 -0500

    Just making sure things are synced

--- coins.py
+++ coins.py
@@ -8,48 +8,49 @@
     flip over all the even-numbered coins, because theyre multiples of 2. Then you'll flip coins No. 3, 6, 9, 12 and so on.
 
 """
-def gen_coins(heads='.', n=100):
-	for i in range(1, n + 1):
-		coins = heads * n
-	return coins
+import termcolor
 
-def flip_coins(coins, tails='·'):
-    flipped_coins = []
+def flip_coins(n, all_flips=False):
+    coins = [x for x in range(n + 1)]
+    coins[0] = None
+    coins.append(None)
 
-    # Make list
-    for c in range(len(coins)):
-        flipped_coins.append(coins[c])
+    flip_coins = ['+' for i in range(n + 1)]
+    flip_coins[0] = ''
 
-    print(flipped_coins)
+    for n in range(1, len(coins) - 1):
+        for z in range(1, n + 1):
+            if (coins[n] % z == 0):
+                flip_coins[n] = flip(flip_coins[n])
+        if all_flips == True:
+            print(''.join(flip_coins))
+    if all_flips == False:
+        print(''.join(flip_coins))
 
-    """
-        for i in range(len(flipped_coins)):
-            #print(i, end='')
-            for n in range(len(flipped_coins)):
-                if i == 0:
-                    flipped_coins[i] = tails
-                    try:
-                        if n % i == 0:
-                            flipped_coins[i] = tails
-                            #print((n % i), end='')
-                    except ZeroDivisionError:
-                        pass
-        print(''.join(flipped_coins))
-    return ''.join(flipped_coins)
-    """
+def flip(coin):
+    if coin == '+':
+        coin = '-'
+    elif coin == '-':
+        coin = '+'
 
-    for n in range(len(coins)):
-        print(flipped_coins[n])
+    return coin
 
+def also_interesting():
+    # Mostly just saving for later
+    # to test and see if solving the problem
+    # may be faster with something kinda
+    # like this
+    for y in range(1, 101):
+        #print(termcolor.colored('.', 'red'))
+        print('Flip#{}: '.format(y), end='')
+
+        #if (y != 1) or (y != 0):
+        flip = [x for x in range(0, 101, y)]
+        print(len(flip))
+        print(flip)
 
 def main():
-	coins = gen_coins(heads = '+', n = 3)
-	#print('')
-	#print(coins)
-
-	flipped_coins = flip_coins(coins, tails = '-')
-	#print('')
-	#print(flipped_coins)
+    coins = flip_coins(100)
 
 if __name__ == '__main__':
     main()
